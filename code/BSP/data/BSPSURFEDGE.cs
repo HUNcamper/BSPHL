e30d92eb3a38f518dda5e62563e7d44d5243da6b
HUNcamper <selimnahimi@gmail.com>
2023-02-20 23:32:38 +0100

    Added GetVertexIndex method to BSPSURFEDGE

--- code/BSP/data/BSPSURFEDGE.cs
+++ code/BSP/data/BSPSURFEDGE.cs
@@ -17,6 +17,21 @@ namespace Sandbox.BSP.data
 			this.value = value;
 		}
 
+		// If the value of the surfedge is positive,
+		// the first vertex of the edge is used as vertex for rendering the face,
+		// otherwise, the value is multiplied by -1 and the second vertex of the indexed edge is used. 
+		public int GetVertexIndex( BSPEDGE[] edgeList )
+		{
+			if (value > 0)
+			{
+				return edgeList[value].iVertex[0];
+			}
+			else
+			{
+				return edgeList[-value].iVertex[1];
+			}
+		}
+
 		public static BSPSURFEDGE FromBytes( byte[] bytes, int offset )
 		{
 			int value = BitConverter.ToInt32( bytes, offset );
